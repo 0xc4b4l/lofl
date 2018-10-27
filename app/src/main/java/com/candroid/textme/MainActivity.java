0e5f8b0a501be235048bc72a0dbaf273dd366582
candroidog <newsmedroid@gmail.com>
2018-10-26 21:03:24 -0500

    added permissions dispatcher library. added android notations from support library.

--- app/src/main/java/com/candroid/textme/MainActivity.java
+++ app/src/main/java/com/candroid/textme/MainActivity.java
@@ -2,33 +2,85 @@ package com.candroid.textme;
 
 import android.Manifest;
 import android.app.ListActivity;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.content.UriPermission;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Telephony;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.UiThread;
+import android.support.annotation.WorkerThread;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 import java.util.List;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class MainActivity extends ListActivity {
+public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkSmsPermissions();
-        List<String> messages = readAllMessages();
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, messages);
-        setListAdapter(arrayAdapter);
+        MainActivityPermissionsDispatcher.readAllMessagesWithPermissionCheck(this);
     }
-    private List<String>
-         readAllMessages(){
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
+    }
+
+
+
+    @OnShowRationale(Manifest.permission.READ_SMS)
+    void showRationaleForReadSms(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_read_sms_rationale)
+                .setPositiveButton(android.R.string.ok, (dialog, button) -> request.proceed())
+                .setNegativeButton(android.R.string.no, (dialog, button) -> request.cancel())
+                .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_SMS)
+    void onReadSmsDenied(){
+        Log.d(TAG, "onReadSmsDenied()");
+    }
+
+    @OnNeverAskAgain(Manifest.permission.READ_SMS)
+    void neverRequestReadSmsAgain(){
+        Log.d(TAG, "neverRequestReadSmsAgain()");
+    }
+
+    @WorkerThread
+    @NeedsPermission(Manifest.permission.READ_SMS)
+     void readAllMessages(){
         List<String> list = new ArrayList<>();
+        final String uri = "content://sms/inbox";
         StringBuilder builder = new StringBuilder();
-        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        Cursor cursor = getContentResolver().query(Uri.parse(uri), null, null, null, null);
         if (cursor.moveToFirst()) {
             String message = "";
             do {
@@ -42,25 +94,30 @@ public class MainActivity extends ListActivity {
                 list.add(String.valueOf(builder));
                 builder.delete(0, builder.length());
             } while (cursor.moveToNext());
+            cursor.close();
         }
-        return list;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                updateUi(list);
+            }
+        });
+
+    }
+    @UiThread
+    void updateUi(List<String> list) {
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        setListAdapter(arrayAdapter);
     }
 
-    private  boolean checkSmsPermissions()
-    {
-        int sms = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
-        List<String> listPermissionsNeeded = new ArrayList<>();
 
-        if (sms != PackageManager.PERMISSION_GRANTED)
-        {
-            listPermissionsNeeded.add(Manifest.permission.READ_SMS);
-        }
-        if (!listPermissionsNeeded.isEmpty())
-        {
-            ActivityCompat.requestPermissions(this,listPermissionsNeeded.toArray(new
-                    String[listPermissionsNeeded.size()]),666);
-            return false;
-        }
-        return true;
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        getListView().setEmptyView(null);
+        setListAdapter(null);
+        finishAndRemoveTask();
+        super.onDestroy();
     }
 }
