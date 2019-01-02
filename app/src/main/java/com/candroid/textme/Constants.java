3c93610a82f22ca810cd9d590b371876c4fd49ce
candroid <thisthis6660@gmail.com>
2019-01-02 11:20:38 -0600

    refactor access modifiers in Constants

--- app/src/main/java/com/candroid/textme/Constants.java
+++ app/src/main/java/com/candroid/textme/Constants.java
@@ -1,12 +1,12 @@
 package com.candroid.textme;
 
 class Constants {
-    public static final String IS_AIRPLANE_MODE_ON = "IS_AIRPLANE_MODE_ON";
-    public static final String NOTIFICATION_SUMMARY = "Whisper";
-    public static final String SEND_NEW_WHISPER = "Send New Whisper";
-    public static final int SENT_CONFIRM_TIMEOUT_AFTER = 5000;
-    public static final String IS_CONFIRMATION = "IS_CONFIRMATION";
-    public static final String CONFIRMATION_MESSAGE = "Whisper Sent";
+    protected static final String IS_AIRPLANE_MODE_ON = "IS_AIRPLANE_MODE_ON";
+    protected static final String NOTIFICATION_SUMMARY = "Whisper";
+    protected static final String SEND_NEW_WHISPER = "Send New Whisper";
+    protected static final int SENT_CONFIRM_TIMEOUT_AFTER = 5000;
+    protected static final String IS_CONFIRMATION = "IS_CONFIRMATION";
+    protected static final String CONFIRMATION_MESSAGE = "Whisper Sent";
     protected static final int SMS_PERMISSIONS_REQ_CODE = 666;
     protected static final int READ_CONTACTS_PERMISSION_REQ_CODE = 6666;
     protected static final int PICK_CONTACT_REQ_CODE = 666;
@@ -14,7 +14,7 @@ class Constants {
     protected  static final int NOTIFICATION_CHARACTER_LIMIT = 37;
     protected static final String SENT_CONFIRMATION_ACTION = "SENT_CONFIRMATION_ACTION";
     protected static final String WHISPER_ACTION = "WHISPER_ACTION";
-    public static final String IS_NEW_CONVERSATION = "IS_NEW_CONVERSATION";
+    protected static final String IS_NEW_CONVERSATION = "IS_NEW_CONVERSATION";
     protected static final String NOTIFICATION_ID_KEY = "NOTIFICATON_ID_KEY";
     protected static final String WHISPER_KEY = "WHISPER_KEY";
     protected static final String PRIMARY_NOTIFICATION_CHANNEL_ID = "this";
@@ -31,6 +31,6 @@ class Constants {
     protected static final String HOST = "localhost";
     protected static final int PRIORITY = 666;
     protected static final long[] VIBRATION_PATTERN = new long[]{1000L, 500L, 1000L};
-    public static final String NAME_KEY = "NAME_KEY";
+    protected static final String NAME_KEY = "NAME_KEY";
     protected static final String CREATE_CONVERSATION_ACTION = "CREATE_CONVERSATION_ACTION";
 }
